ceb8fc68717514e73a35351ed00408989f9bc6f9
nadz <leonard.ucarino@gmail.com>
2020-06-01 13:15:11 +0800

    added offline data to be retrive when no interenet connection

--- src/main/java/com/forex/service/ForexService.java
+++ src/main/java/com/forex/service/ForexService.java
@@ -1,10 +1,14 @@
 package com.forex.service;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -18,17 +22,49 @@ public class ForexService {
 	@Autowired
 	private RestTemplate restTemplate;
 
-	public List<Rate> getForexData(String base) {
+	@Autowired
+	private JSONParser parser;
+
+	public List<Rate> getOnlineData(String base) {
 
 		String url = ForexConstants.BASE_URL + base;
-		JSONParser parser = new JSONParser();
 		List<Rate> rateList = new ArrayList<Rate>();
 		try {
 			String result = this.restTemplate.getForObject(url, String.class);
-			JSONObject object = (JSONObject) parser.parse(result);
+			JSONObject object = (JSONObject) this.parser.parse(result);
+			this.writeFile(object.toJSONString());
+			rateList = this.rateMapper(object);
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		return rateList;
+	}
+
+	public void writeFile(String data) {
+		FileWriter file = null;
+		try {
+			file = new FileWriter("default_data.json");
+			file.write(data);
+		} catch (IOException e) {
+			System.out.println(e);
+		} finally {
+			try {
+				file.close();
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+		}
+
+	}
+
+	public List<Rate> rateMapper(JSONObject object) {
+		List<Rate> rateList = new ArrayList<Rate>();
+		if (!object.isEmpty()) {
 			String date = object.get("date").toString();
 			Object[] keys = ((JSONObject) object.get("rates")).keySet().toArray();
-			Object[] values = ((JSONObject) object.get("rates")).values().toArray();			
+			Object[] values = ((JSONObject) object.get("rates")).values().toArray();
 			for (int i = 0; i < keys.length; i++) {
 				Rate rate = new Rate();
 				rate.setTarget(keys[i].toString());
@@ -36,9 +72,27 @@ public class ForexService {
 				rate.setRate(Double.parseDouble(ForexConstants.DECIMAL_FORMAT.format(values[i])));
 				rateList.add(rate);
 			}
+		}
+		return rateList;
+	}
 
-		} catch (Exception e) {
+	public List<Rate> getOfflineData() {
+		List<Rate> rateList = new ArrayList<Rate>();		
+		FileReader reader = null;
+		
+		try {
+			 reader = new FileReader("default_data.json");
+			JSONObject object = (JSONObject) this.parser.parse(reader);
+			rateList = this.rateMapper(object);
+
+		} catch (IOException | ParseException e) {
 			e.printStackTrace();
+		} finally {
+			try {				
+				reader.close();
+			} catch (IOException e) {
+				System.out.println(e);
+			}
 		}
 
 		return rateList;
